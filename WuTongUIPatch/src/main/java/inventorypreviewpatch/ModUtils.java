501a82f8adaed5763bc0435b1355b15ff3d6f56c
lingyunLY <2437576120@qq.com>
2025-09-14 17:24:28 +0800

    Add files via upload

---
+++ WuTongUIPatch/src/main/java/inventorypreviewpatch/ModUtils.java
@@ -0,0 +1,44 @@
+package inventorypreviewpatch;
+
+import fi.dy.masa.malilib.interfaces.IDataSyncer;
+import fi.dy.masa.minihud.data.EntitiesDataManager;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.render.BufferBuilder;
+
+public class ModUtils {
+
+    public static boolean isGenericScreen(Screen screen) {
+        boolean isGenericScreen = false;
+        if (screen instanceof HandledScreen<?>) {
+            isGenericScreen = !(screen instanceof CreativeInventoryScreen) && !(screen instanceof InventoryScreen);
+        }
+        return isGenericScreen;
+    }
+
+    //同步器，借鉴于malilib
+    public static IDataSyncer getDataSyncer(IDataSyncer syncer) {
+        if (syncer == null) {
+            syncer = EntitiesDataManager.getInstance();
+        }
+        return syncer;
+    }
+
+    //渲染
+    public static void drawTexturedRectBatched(int x, int y, int u, int v, int width, int height, float pixelWidth, BufferBuilder buffer) {
+        drawTexturedRectBatched(x, y, u, v, width, height, 0, pixelWidth, buffer);
+    }
+
+    public static void drawTexturedRectBatched(int x, int y, int u, int v, int width, int height, float zLevel, float pixelWidth, BufferBuilder buffer) {
+        //pixelWidth即像素宽度应为1.0与材质文件边长的比值
+        if (pixelWidth == 0) pixelWidth = 0.00390625F;
+
+        buffer.vertex(x, y + height, zLevel).texture(u * pixelWidth, (v + height) * pixelWidth);
+        buffer.vertex(x + width, y + height, zLevel).texture((u + width) * pixelWidth, (v + height) * pixelWidth);
+        buffer.vertex(x + width, y, zLevel).texture((u + width) * pixelWidth, v * pixelWidth);
+        buffer.vertex(x, y, zLevel).texture(u * pixelWidth, v * pixelWidth);
+    }
+
+}
